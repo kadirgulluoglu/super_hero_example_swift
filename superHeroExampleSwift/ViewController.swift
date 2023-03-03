6e00a626793c2fe35e08c9ed1b07e0e3b85b2cbe
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-03-03 17:33:58 +0300

    add segue and tableview features
    add segue because I send data between page. data is super hero name and image path.

--- superHeroExampleSwift/ViewController.swift
+++ superHeroExampleSwift/ViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 
 class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource
 {
+    var superHeroName = [String()]
+    var superHeroImage = [String()]
+    var selectedName = ""
+    var selectedImage = ""
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         //Resimlerin adlarını listeye ekledik
-        var superHeroName = [String()]
         superHeroName.append("Batman")
         superHeroName.append("Superman")
         superHeroName.append("Hulk")
         superHeroName.append("Icardi")
-        //Resimlerin pathleri
-        var superHeroImage = [String()]
+        
+        //Resimlerin pathleri listeye eklendi
         superHeroImage.append("batman")
         superHeroImage.append("superman")
         superHeroImage.append("hulk")
@@ -31,13 +34,42 @@ class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSourc
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 
+        return superHeroName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = superHeroName[indexPath.row]
         return cell
+    }
+    
+    
+     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+         if editingStyle == .delete {
+             superHeroName.remove(at: indexPath.row)
+             superHeroImage.remove(at: indexPath.row)
+             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.left)
+         }
+                
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+         selectedImage = superHeroImage[indexPath.row]
+         selectedName = superHeroName[indexPath.row]
+        
+        
+        
+        performSegue(withIdentifier: "toSuperHero", sender: nil)
+        
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSuperHero"{
+            let destinationVC = segue.destination as! SuperHeroDetailsController
+            destinationVC.selectedHeroImage = selectedImage
+            destinationVC.selectedHeroName = selectedName
+        }
     }
     
 }
